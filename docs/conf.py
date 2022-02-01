d60639a44b03209c6dde0bde333d49bbaca426e5
Panagiotis Simakis <panagiotis@netdata.cloud>
2022-02-01 16:12:45 +0200

    update rtd config

--- docs/conf.py
+++ docs/conf.py
@@ -13,16 +13,16 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import typing
 
 # -- Project information -----------------------------------------------------
 
-project = 'scrapy-folder-tree'
-copyright = '2022, Panagiotis Simakis'
-author = 'Panagiotis Simakis'
+project = "scrapy-folder-tree"
+copyright = "2022, Panagiotis Simakis"
+author = "Panagiotis Simakis"
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.3'
+release = "v0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,15 +30,15 @@ release = 'v0.1.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions: typing.List[str] = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,12 +46,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_favicon = "_static/black-logo.png"
 
 html_theme_options = {
