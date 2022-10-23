a5042455f0f36ee474242ee09ed31786cbb220cb
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-10-23 11:50:25 +0530

    Introduction to `FOLDER_TREE_LENGTH` config parameter
    Resolves #1.
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- src/scrapy_folder_tree/trees/hash.py
+++ src/scrapy_folder_tree/trees/hash.py
@@ -4,10 +4,10 @@ from . import TreeBase
 
 
 class HashTree(TreeBase):
-    def __init__(self, depth: int) -> None:
+    def __init__(self, depth: int, length: int) -> None:
         self.DEPTH = depth
+        self.LENGTH = length
 
     def build_path(self, filepath) -> str:
         filename = os.path.basename(filepath)
-        dir_name = os.path.dirname(filepath)
-        return os.path.join(dir_name, *[_ for _ in filename[: self.DEPTH]], filename)
+        return os.path.join(*[filename[i:i + self.LENGTH] for i in range(0, self.DEPTH * self.LENGTH, self.LENGTH)], filename)
